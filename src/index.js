2f8226f16a8f16f02a8bfbe6bfbbed04ff50b112
Jeff Israel <jeffrey_israel@condenast.com>
2018-07-12 17:41:17 -0500

    Move Slack specific stuff into a separate module

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 const debug = require('debug')('csl-app');
 const logError = require('debug')('csl-app:error');
-const { WebClient } = require('@slack/client');
+const { Slack } = require('./slack');
 
-
-const slackWebClient = new WebClient(process.env.SLACK_APP_TOKEN);
-
-function updateStatus(userId, statusEmoji, statusText) {
-  const data = {
-    user: userId,
-    profile: {
-      status_emoji: statusEmoji || '',
-      status_text: statusText || ''
-    }
-  };
-  return slackWebClient.users.profile.set(data);
-}
+const slack = new Slack(
+  process.env.SLACK_APP_TOKEN,
+  process.env.SLACK_BOT_TOKEN);
 
 if (process.env.SLACK_USER_ID) {
-  updateStatus(process.env.SLACK_USER_ID, ':nerd_face:', `hacking the gibson at ${new Date()}`)
+  slack.updateStatus(process.env.SLACK_USER_ID, ':nerd_face:', `hacking the gibson at ${new Date()}`)
     .then((slackRes) => {
       debug(JSON.stringify(slackRes.profile, undefined, 2));
     })
@@ -25,3 +15,5 @@ if (process.env.SLACK_USER_ID) {
       logError(err);
     });
 }
+
+exports.slack = slack;
