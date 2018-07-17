c8f3f819eec76123121784237672f7ac412d36bf
Jeff Israel <jeffrey_israel@condenast.com>
2018-07-17 13:51:07 -0500

    Typos from PR #2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Cryptic Son Lyrics
+# Cryptic Song Lyrics
 
 A throwback to the days of AIM: a Slack application to save and reuse status messages
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ const debug = require('debug')('csl-app');
 const logError = require('debug')('csl-app:error');
 const { Slack } = require('./slack');
 
-const slack = new Slack(
-  process.env.SLACK_APP_TOKEN,
-  process.env.SLACK_BOT_TOKEN);
+const slack = new Slack(process.env.SLACK_APP_TOKEN);
 
 if (process.env.SLACK_USER_ID) {
   slack.updateStatus(process.env.SLACK_USER_ID, ':nerd_face:', `hacking the gibson at ${new Date()}`)
